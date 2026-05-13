a41e62d4dd52d530c0884c5f7dc76e595e8abe51
Thomas Fraissl <fraissl@gmx.net>
2026-05-13 19:18:12 +0200

    new FIRMWARE_VERSION & FIRMWARE_BUILD_DATE

--- examples/simple_repeater/MyMesh.h
+++ examples/simple_repeater/MyMesh.h
@@ -69,11 +69,11 @@ struct NeighbourInfo {
 };
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "19 Apr 2026"
+  #define FIRMWARE_BUILD_DATE __DATE__ " " __TIME__
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.15.0"
+  #define FIRMWARE_VERSION   "v1.15.0 FLOW"
 #endif
 
 #define FIRMWARE_ROLE "repeater"
