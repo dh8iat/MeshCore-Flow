fca804a28ed1835f0538b445af21b8a1ad912324
Thomas Fraissl <fraissl@gmx.net>
2026-06-07 23:27:05 +0200

    modified:   docs/interesting-prs.md 	modified:   examples/simple_room_server/MyMesh.cpp

--- examples/simple_room_server/MyMesh.cpp
+++ examples/simple_room_server/MyMesh.cpp
@@ -646,7 +646,7 @@ MyMesh::MyMesh(mesh::MainBoard &board, mesh::Radio &radio, mesh::MillisecondCloc
   _prefs.disable_fwd = 1;
   _prefs.advert_interval = 1;        // default to 2 minutes for NEW installs
   _prefs.flood_advert_interval = 12; // 12 hours
-  _prefs.flood_advert_base = 0.308f;
+  _prefs.flood_max_advert = 8;
   _prefs.flood_max = 64;
   _prefs.interference_threshold = 1; // non-zero enables hardware CAD before TX
 #ifdef ROOM_PASSWORD
